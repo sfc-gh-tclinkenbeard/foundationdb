1e60e7ec0f380ab1219f4926d394c80448df5d4c
sfc-gh-tclinkenbeard <trevor.clinkenbeard@snowflake.com>
2021-02-19 10:24:40 -0800

    Increase default TLOG_MAX_CREATE_DURATION to 120.0

--- fdbserver/Knobs.cpp
+++ fdbserver/Knobs.cpp
@@ -87,7 +87,7 @@ void ServerKnobs::initialize(bool randomize, ClientKnobs* clientKnobs, bool isSi
 	init( MAX_CACHE_VERSIONS,                                   10e6 );
 	init( TLOG_IGNORE_POP_AUTO_ENABLE_DELAY,                   300.0 );
 	init( TXS_POPPED_MAX_DELAY,                                  1.0 ); if ( randomize && BUGGIFY ) TXS_POPPED_MAX_DELAY = deterministicRandom()->random01();
-	init( TLOG_MAX_CREATE_DURATION,                             10.0 );
+	init( TLOG_MAX_CREATE_DURATION,                            120.0 ); if ( randomize && BUGGIFY ) TLOG_MAX_CREATE_DURATION = 10.0;
 	init( PEEK_LOGGING_AMOUNT,                                     5 );
 	init( PEEK_LOGGING_DELAY,                                    5.0 );
 	init( PEEK_RESET_INTERVAL,                                 300.0 ); if ( randomize && BUGGIFY ) PEEK_RESET_INTERVAL = 20.0;
